a2c4cdbab7737ee89a1b7de9f789d52c2368ee73
dylan-kiew <dylanksl@yahoo.com>
2022-05-04 14:18:20 +0800

    implement ParseToJson() in User struct

--- data/models/types.go
+++ data/models/types.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type User struct {
 	Id       string
 	Email    string
@@ -15,5 +17,32 @@ type Name struct {
 }
 
 func (u User) ParseToJson() string {
-	return "{}"
+
+	content := "{\n\t"
+	content += fmt.Sprintf("\"id\" : \"%s\", \n\t", u.Id)
+	content += fmt.Sprintf("\"email\" : \"%s\", \n\t", u.Email)
+	content += fmt.Sprintf("\"isActive\" : %v, \n\t", u.IsActive)
+	content += fmt.Sprintf("\"name\" : { \n\t\"first\" :\" %s\", \n\t\"last\" : \"%s\"\n\t}, ", u.Name.First, u.Name.Last)
+
+	// handle roles attribure
+	if len(u.Roles) == 0 {
+		// empty roles attribute
+		content += fmt.Sprintln("\"roles\" : []")
+	} else {
+		// non-empty roles attribute
+		roles := ""
+		count := 1
+		for _, r := range u.Roles {
+			if count == 1 {
+				roles += fmt.Sprintf("\"%s\"\n\t", r)
+			} else {
+				roles += fmt.Sprintf(", \"%s\"\n\t", r)
+			}
+			count++
+		}
+		content += fmt.Sprintf("\"roles\" : [%s]", roles)
+	}
+	content += fmt.Sprintln("\n\t}")
+	//
+	return content
 }
--- users/users.go
+++ users/users.go
@@ -65,36 +65,11 @@ func GetAll(w http.ResponseWriter, r *http.Request) {
 		user, _ := accounts.Pop()
 		if count == 1 {
 			// first user data point
-			content += "{"
+			content += user.ParseToJson()
 		} else {
 			// more user data point
-			content += ", {"
+			content += fmt.Sprintf(", %s", user.ParseToJson())
 		}
-
-		content += fmt.Sprintf("\"id\" : \"%s\", ", user.Id)
-		content += fmt.Sprintf("\"email\" : \"%s\", ", user.Email)
-		content += fmt.Sprintf("\"isActive\" : %v, ", user.IsActive)
-		content += fmt.Sprintf("\"name\" : { \"first\" :\" %s\", \"last\" : \"%s\"}, ", user.Name.First, user.Name.Last)
-
-		// handle roles attribure
-		if len(user.Roles) == 0 {
-			// empty roles attribute
-			content += fmt.Sprintln("\"roles\" : []")
-		} else {
-			// non-empty roles attribute
-			roles := ""
-			count := 1
-			for _, r := range user.Roles {
-				if count == 1 {
-					roles += fmt.Sprintf("\"%s\"", r)
-				} else {
-					roles += fmt.Sprintf(", \"%s\"", r)
-				}
-				count++
-			}
-			content += fmt.Sprintf("\"roles\" : [%s]", roles)
-		}
-		content += fmt.Sprintln("}")
 		count++
 		// fmt.Println(user)
 		// fmt.Println()
